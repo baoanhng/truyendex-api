33c4786ab69c36142d1680b43ec235b73b3bf088
Bao <baona1995@gmail.com>
2024-10-13 22:33:55 +0700

    User read list

--- app/Http/Controllers/Pub/UserController.php
+++ app/Http/Controllers/Pub/UserController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Pub;
 
 use App\Http\Controllers\Controller;
+use App\Models\ReadList;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function readList(Request $request)
+    {
+        $list = ReadList::where('user_id', $request->user()->id)
+            ->latest('id')
+            ->paginate(20);
+
+        return $list;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Pub\SeriesController;
+use App\Http\Controllers\Pub\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,8 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+
+    Route::get('/user/read-list', [UserController::class, 'readList']);
 
     Route::post('/series/follow', [SeriesController::class, 'follow']);
 });
