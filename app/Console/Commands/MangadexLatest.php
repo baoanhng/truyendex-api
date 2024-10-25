42589718306bb752ef6f087852fca4422479178a
Bao <baona1995@gmail.com>
2024-10-25 17:15:24 +0700

    Cron to get latest mangadex chapters and create regarding series.

---
+++ app/Console/Commands/MangadexLatest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Series;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+
+class MangadexLatest extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cron:mangadex-latest';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update or Create Series based on latest chapters on Mangadex';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $response = Http::get('https://api.mangadex.org/chapter?limit=10&translatedLanguage%5B%5D=vi&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&includeFutureUpdates=0&includeFuturePublishAt=0&includeExternalUrl=0&order%5BreadableAt%5D=desc&includes%5B%5D=manga');
+
+        if (!$response->ok()) {
+            $this->error('Http code not ok');
+            return;
+        }
+
+        $data = $response->json();
+
+        if ($data['result'] !== 'ok') {
+            $this->error('Returned result code is not OK');
+            return;
+        }
+
+        $chapters = $data['data'];
+
+        foreach ($chapters as $chapter) {
+            $seriesId = $chapter['relationships'][1]['id'];
+
+            Series::updateOrCreate(['uuid' => $seriesId], [
+                'uuid' => $seriesId,
+                'latest_chapter_uuid' => $chapter['id'],
+            ]);
+        }
+    }
+}
