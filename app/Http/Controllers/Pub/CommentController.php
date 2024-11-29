b6a38e27e195d8f182ca4b5f66f9adfb02ced2f4
Bao <baona1995@gmail.com>
2024-11-29 23:36:25 +0700

    Save

---
+++ app/Http/Controllers/Pub/CommentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Pub;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+    public function store(Request $request)
+    {
+
+    }
+}
