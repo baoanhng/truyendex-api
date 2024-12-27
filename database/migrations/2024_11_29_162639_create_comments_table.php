17f1f5fbb2bf32ec1030bcedc45bda8b82f7389f
Bao <baona1995@gmail.com>
2024-12-27 21:52:02 +0700

    UUID morph comment

--- database/migrations/2024_11_29_162639_create_comments_table.php
+++ database/migrations/2024_11_29_162639_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->morphs('commentable');
+            $table->uuidMorphs('commentable');
             $table->unsignedBigInteger('parent_id');
             $table->mediumText('content');
             $table->timestamps();
