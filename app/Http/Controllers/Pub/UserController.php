52902960933f570f286b1c4c01deee830770848e
zennomi <zennomi.kun@gmail.com>
2025-01-28 11:42:16 +0700

    Sync list api

--- app/Http/Controllers/Pub/UserController.php
+++ app/Http/Controllers/Pub/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Pub;
 use App\Http\Controllers\Controller;
 use App\Models\ReadList;
 use App\Services\ReadListService;
+use Http;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -19,5 +20,44 @@ class UserController extends Controller
         $list = ReadListService::userList($request->user());
 
         return $list;
+    }
+
+    /**
+     *
+     * @param Request $request
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function syncList(Request $request)
+    {
+        $validated = $request->validate([
+            'source' => ['required', 'string'],
+            'ids' => ['required', 'array'],
+            'ids.*' => ['required', 'string'],
+        ]);
+
+        $response = Http::get('https://directus.truyendex.xyz/directus-endpoint-truyendex/map', [
+            'source' => $validated['source'],
+            'ids' => implode(',', $validated['ids']),
+        ]);
+
+        if ($response->failed()) {
+            return response()->json([
+                'message' => 'Failed to sync list',
+            ], 500);
+        }
+
+        $data = $response->json(); // {"result":{"2101":["fee73e75-075d-423a-b597-5d96cd9cb554"],"2147":["ab541412-f0d0-4fcf-a7a3-084a835d9d4a"]}}
+
+        $uuids = collect($data['result'])->flatten();
+
+        $inserts = $uuids->map(function ($uuid) use ($request) {
+            return ['user_id' => $request->user()->id, 'series_uuid' => $uuid];
+        });
+
+        $result = ReadList::upsert($inserts->toArray(), ['user_id', 'series_uuid']);
+
+        return response()->json([
+            'followed' => $result,
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
     });
 
     Route::get('/user/read-list', [UserController::class, 'readList']);
+    Route::post('/user/read-list/sync', [UserController::class, 'syncList']);
 
     Route::post('/series/check-follow', [SeriesController::class, 'checkFollow']);
     Route::post('/series/follow', [SeriesController::class, 'follow']);
