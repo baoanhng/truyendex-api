c0458673100430c56961a949beeb915eaa70228b
Bao <baona1995@gmail.com>
2025-02-01 11:42:59 +0700

    Verify email at register

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // VerifyEmail::createUrlUsing(function (object $notifiable) {
+        //     $hash = sha1($notifiable->getEmailForVerification());
+        //     return config('app.frontend_url')."/verify-email/{$notifiable->getKey()}/{$hash}";
+        // });
+
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
