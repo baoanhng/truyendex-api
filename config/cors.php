ec2a62fb37330f91122718b88532cc36dd6fb5c2
Bao <baona1995@gmail.com>
2025-02-01 15:10:36 +0700

    Sanctum stateful domain

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), ...array_filter(explode(',', env('FRONTEND_CORS_ORIGINS')))],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), ...array_filter(explode(',', env('FRONTEND_ALT_URLS')))],
 
     'allowed_origins_patterns' => [],
 
--- config/sanctum.php
+++ config/sanctum.php
@@ -16,10 +16,11 @@ return [
     */
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s%s',
+        '%s%s%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
-        env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
+        env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : '',
+        env('FRONTEND_ALT_URLS') ? ','.implode(',', array_map(fn($url) => parse_url($url, PHP_URL_HOST), explode(',', env('FRONTEND_ALT_URLS')))) : ''
     ))),
 
     /*
