bcba2bdb1bcb21daaeb92eca572ab2e5112d8e9b
Bao <baona1995@gmail.com>
2025-02-01 16:06:34 +0700

    Save user last activity time

---
+++ app/Http/Middleware/SaveUserLastActivity.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SaveUserLastActivity
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        if ($user && ($user->last_activity_at ?? $user->created_at)->diffInMinutes() > 5) {
+            $user->timestamps = false;
+            $user->last_activity_at = now();
+            $user->save();
+        }
+
+        return $next($request);
+    }
+}
