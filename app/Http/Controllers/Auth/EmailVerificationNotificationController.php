3f79c086ac1aa99520f6b0bfa5ed32e4f095ed3f
Bao <baona1995@gmail.com>
2025-02-01 16:14:33 +0700

    Captcha for resend email verification

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class EmailVerificationNotificationController extends Controller
 {
@@ -14,6 +15,10 @@ class EmailVerificationNotificationController extends Controller
      */
     public function store(Request $request): JsonResponse|RedirectResponse
     {
+        $request->validate([
+            'cf-turnstile-response' => ['required', Rule::turnstile()],
+        ]);
+
         if ($request->user()->hasVerifiedEmail()) {
             return redirect()->intended('/');
         }
