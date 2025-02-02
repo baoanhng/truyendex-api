daf7de0a34f369f4b4b0e0bf98d0898370f316ea
Bao <baona1995@gmail.com>
2025-02-02 17:05:00 +0700

    Set user role command

---
+++ app/Console/Commands/SetUserRole.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\RolesEnum;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class SetUserRole extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:set-user-role {user? : UserID} {role? : RoleName}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Assign a role to a user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $userId = $this->argument('user') ?? $this->ask('Enter the user ID');
+        $roleName = $this->argument('role') ?? $this->choice('Enter the role name', RolesEnum::values(), default: 2, multiple: false);
+
+        $user = User::find($userId);
+        $user->assignRole($roleName);
+
+        $this->info("Role {$roleName} assigned to user {$user->name}");
+    }
+}
--- app/Enums/RolesEnum.php
+++ app/Enums/RolesEnum.php
@@ -7,6 +7,7 @@ enum RolesEnum: string
     case ADMIN = 'admin';
     case MODERATOR = 'moderator';
     case MEMBER = 'member';
+    case EXILE = 'exile';
 
     // Display name
     public function label(): string
@@ -15,6 +16,12 @@ enum RolesEnum: string
             static::ADMIN => 'Admin',
             static::MODERATOR => 'Moderator',
             static::MEMBER => 'Member',
+            static::EXILE => 'Exile',
         };
+    }
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
     }
 }
