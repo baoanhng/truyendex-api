77bd1543c829f9890abbd2030227ad36b6f613cd
Bao <baona1995@gmail.com>
2025-02-02 13:45:11 +0700

    Add cmt count chapters table

---
+++ database/migrations/2025_02_02_064326_add_comment_count_chapters_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('chapters', function (Blueprint $table) {
+            $table->unsignedBigInteger('comment_count')->default(0)->after('series_uuid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('chapters', function (Blueprint $table) {
+            $table->dropColumn('comment_count');
+        });
+    }
+};
