7859c46e1838bb09cad9080732db8883947b8c09
Bao <baona1995@gmail.com>
2025-02-04 21:25:59 +0700

    Socialite

--- app/Helper.php
+++ app/Helper.php
@@ -13,4 +13,16 @@ class Helper
     {
         return request()->getScheme() . '://' . ltrim(str_replace('api', '', request()->getHost()), '.');
     }
+
+    /**
+     *
+     * @param string $provider
+     * @return void
+     */
+    public static function verifySocialiteProvider(string $provider)
+    {
+        if (!in_array($provider, ['google'])) {
+            abort(400, 'Invalid provider');
+        }
+    }
 }
--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Helper;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Socialite;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -32,6 +35,38 @@ class AuthenticatedSessionController extends Controller
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
+
+        return response()->noContent();
+    }
+
+    /**
+     *
+     * @param string $provider
+     * @return Response
+     */
+    public function socialiteRedirect(string $provider): Response
+    {
+        Helper::verifySocialiteProvider($provider);
+
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     *
+     * @param string $provider
+     * @return Response
+     */
+    public function socialiteCallback(string $provider): Response
+    {
+        Helper::verifySocialiteProvider($provider);
+
+        $socUser = Socialite::driver($provider)->user();
+
+        if (!$socUser || !$socUser->email) {
+            abort(400);
+        }
+
+        $user = User::where('email', $socUser->email)->first();
 
         return response()->noContent();
     }
