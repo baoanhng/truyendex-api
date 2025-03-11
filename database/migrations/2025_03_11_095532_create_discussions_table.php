63c1c0d04d555bb948b217b0357ce6f5f7b7a432
Bao <baona1995@gmail.com>
2025-03-11 22:22:06 +0700

    Fix migration

--- database/migrations/2025_03_11_095532_create_discussions_table.php
+++ database/migrations/2025_03_11_095532_create_discussions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('discussions', function (Blueprint $table) {
             $table->id();
-            $table->unique('uuid');
+            $table->uuid('uuid')->unique();
             $table->unsignedBigInteger('user_id');
             $table->string('title');
             $table->string('slug');
